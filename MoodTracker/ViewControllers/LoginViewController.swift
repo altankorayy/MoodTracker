b266bab2de2af91f7ed135a7236721c7673d23ba
Altan Koray Benli <altankoraybenli@gmail.com>
2023-08-22 16:09:32 +0300

    loginVC and registerVC done

--- MoodTracker/ViewControllers/LoginViewController.swift
+++ MoodTracker/ViewControllers/LoginViewController.swift
@@ -8,17 +8,84 @@
 import UIKit
 
 class LoginViewController: UIViewController {
+    
+    private let emailTextField: UITextField = {
+        let textField = UITextField()
+        textField.translatesAutoresizingMaskIntoConstraints = false
+        textField.keyboardType = .default
+        let attributes: [NSAttributedString.Key: Any] = [
+            .foregroundColor: UIColor.white,
+            .font: UIFont.boldSystemFont(ofSize: 16)
+        ]
+        textField.attributedPlaceholder = NSAttributedString(string: "✉️ Email Adress", attributes: attributes)
+        return textField
+    }()
+    
+    private let passwordTextField: UITextField = {
+        let textField = UITextField()
+        textField.translatesAutoresizingMaskIntoConstraints = false
+        textField.keyboardType = .default
+        let attributes: [NSAttributedString.Key: Any] = [
+            .foregroundColor: UIColor.white,
+            .font: UIFont.boldSystemFont(ofSize: 16)
+        ]
+        textField.attributedPlaceholder = NSAttributedString(string: "🔐 Password", attributes: attributes)
+        return textField
+    }()
+    
+    private let loginButton: UIButton = {
+        let button = UIButton()
+        button.translatesAutoresizingMaskIntoConstraints = false
+        button.setTitle("Login", for: .normal)
+        button.setTitleColor(.black, for: .normal)
+        button.titleLabel?.font = .systemFont(ofSize: 16, weight: .bold)
+        button.backgroundColor = .white
+        button.tintColor = .black
+        button.layer.cornerRadius = 16
+        button.layer.masksToBounds = true
+        return button
+    }()
 
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        view.backgroundColor = .blue
+        title = "Login"
+        view.backgroundColor = UIColor(red: 104/255, green: 52/255, blue: 212/255, alpha: 1)
         navigationController?.navigationBar.tintColor = .white
+        navigationController?.navigationBar.titleTextAttributes = [NSAttributedString.Key.foregroundColor: UIColor.white]
+        
+        view.addSubview(emailTextField)
+        view.addSubview(passwordTextField)
+        view.addSubview(loginButton)
+        
+        setConstraints()
     }
     
-    @objc private func didTapBackButton() {
-        dismiss(animated: true)
+    private func setConstraints() {
+        let emailTextFieldConstraints = [
+            emailTextField.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 20),
+            emailTextField.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant: 50),
+            emailTextField.widthAnchor.constraint(equalToConstant: 280),
+            emailTextField.heightAnchor.constraint(equalToConstant: 50)
+        ]
+        
+        let passwordTextFieldConstraints = [
+            passwordTextField.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 20),
+            passwordTextField.topAnchor.constraint(equalTo: emailTextField.bottomAnchor, constant: 20),
+            passwordTextField.widthAnchor.constraint(equalToConstant: 280),
+            passwordTextField.heightAnchor.constraint(equalToConstant: 50)
+        ]
+        
+        let loginButtonConstraints = [
+            loginButton.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+            loginButton.topAnchor.constraint(equalTo: passwordTextField.bottomAnchor, constant: 150),
+            loginButton.widthAnchor.constraint(equalToConstant: 280),
+            loginButton.heightAnchor.constraint(equalToConstant: 50)
+        ]
+        
+        NSLayoutConstraint.activate(emailTextFieldConstraints)
+        NSLayoutConstraint.activate(passwordTextFieldConstraints)
+        NSLayoutConstraint.activate(loginButtonConstraints)
     }
-   
 
 }
--- MoodTracker/ViewControllers/RegisterViewController.swift
+++ MoodTracker/ViewControllers/RegisterViewController.swift
@@ -8,13 +8,104 @@
 import UIKit
 
 class RegisterViewController: UIViewController {
+    
+    private let usernameTextField: UITextField = {
+        let textField = UITextField()
+        textField.translatesAutoresizingMaskIntoConstraints = false
+        textField.keyboardType = .default
+        let attributes: [NSAttributedString.Key: Any] = [
+            .foregroundColor: UIColor.white,
+            .font: UIFont.boldSystemFont(ofSize: 16)
+        ]
+        textField.attributedPlaceholder = NSAttributedString(string: "🙂 Name and Surname", attributes: attributes)
+        return textField
+    }()
+    
+    private let emailTextField: UITextField = {
+        let textField = UITextField()
+        textField.translatesAutoresizingMaskIntoConstraints = false
+        textField.keyboardType = .default
+        let attributes: [NSAttributedString.Key: Any] = [
+            .foregroundColor: UIColor.white,
+            .font: UIFont.boldSystemFont(ofSize: 16)
+        ]
+        textField.attributedPlaceholder = NSAttributedString(string: "✉️ Email Adress", attributes: attributes)
+        return textField
+    }()
+    
+    private let passwordTextField: UITextField = {
+        let textField = UITextField()
+        textField.translatesAutoresizingMaskIntoConstraints = false
+        textField.keyboardType = .default
+        let attributes: [NSAttributedString.Key: Any] = [
+            .foregroundColor: UIColor.white,
+            .font: UIFont.boldSystemFont(ofSize: 16)
+        ]
+        textField.attributedPlaceholder = NSAttributedString(string: "🔐 Password", attributes: attributes)
+        return textField
+    }()
+    
+    private let completeButton: UIButton = {
+        let button = UIButton()
+        button.translatesAutoresizingMaskIntoConstraints = false
+        button.setTitle("Complete", for: .normal)
+        button.setTitleColor(.black, for: .normal)
+        button.titleLabel?.font = .systemFont(ofSize: 16, weight: .bold)
+        button.backgroundColor = .white
+        button.tintColor = .black
+        button.layer.cornerRadius = 16
+        button.layer.masksToBounds = true
+        return button
+    }()
 
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        view.backgroundColor = .red
+        title = "Sign Up"
+        view.backgroundColor = UIColor(red: 104/255, green: 52/255, blue: 212/255, alpha: 1)
         navigationController?.navigationBar.tintColor = .white
+        navigationController?.navigationBar.titleTextAttributes = [NSAttributedString.Key.foregroundColor: UIColor.white]
+        view.addSubview(usernameTextField)
+        view.addSubview(emailTextField)
+        view.addSubview(passwordTextField)
+        view.addSubview(completeButton)
+        
+        setConstraints()
     }
-
-
+    
+    private func setConstraints() {
+        
+        let usernameTextFieldConstraints = [
+            usernameTextField.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 20),
+            usernameTextField.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant: 50),
+            usernameTextField.widthAnchor.constraint(equalToConstant: 280),
+            usernameTextField.heightAnchor.constraint(equalToConstant: 50)
+        ]
+        
+        let emailTextFieldConstraints = [
+            emailTextField.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 20),
+            emailTextField.topAnchor.constraint(equalTo: usernameTextField.bottomAnchor, constant: 20),
+            emailTextField.widthAnchor.constraint(equalToConstant: 280),
+            emailTextField.heightAnchor.constraint(equalToConstant: 50)
+        ]
+        
+        let passwordTextFieldConstraints = [
+            passwordTextField.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 20),
+            passwordTextField.topAnchor.constraint(equalTo: emailTextField.bottomAnchor, constant: 20),
+            passwordTextField.widthAnchor.constraint(equalToConstant: 280),
+            passwordTextField.heightAnchor.constraint(equalToConstant: 50)
+        ]
+        
+        let completeButtonConstraints = [
+            completeButton.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+            completeButton.topAnchor.constraint(equalTo: passwordTextField.bottomAnchor, constant: 150),
+            completeButton.widthAnchor.constraint(equalToConstant: 280),
+            completeButton.heightAnchor.constraint(equalToConstant: 50)
+        ]
+        
+        NSLayoutConstraint.activate(usernameTextFieldConstraints)
+        NSLayoutConstraint.activate(emailTextFieldConstraints)
+        NSLayoutConstraint.activate(passwordTextFieldConstraints)
+        NSLayoutConstraint.activate(completeButtonConstraints)
+    }
 }
